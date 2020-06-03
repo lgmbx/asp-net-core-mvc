1baa25b32541cd58257b94f212ca31875ebff8fd
lgmbx <gustavo_x@live.com>
2020-06-03 20:40:07 -0300

    Department deleted and created again using scaffolding

--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebSalesMVC.Data;
 
 namespace WebSalesMVC {
     public class Startup {
@@ -28,6 +30,9 @@ namespace WebSalesMVC {
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+    services.AddDbContext<WebSalesMVCContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("WebSalesMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
