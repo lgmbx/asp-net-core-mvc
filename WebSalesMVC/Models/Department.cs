22c7347e43ca4bd4286336a3fcc12d83f135a4d4
lgmbx <gustavo_x@live.com>
2020-06-04 15:37:01 -0300

    other entities and DB update

--- WebSalesMVC/Models/Department.cs
+++ WebSalesMVC/Models/Department.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 namespace WebSalesMVC.Models {
     public class Department {
 
+        #region Properties
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        #endregion
+
+        #region Constructors
+        public Department() {
+        }
+
+        public Department(int id, string name) {
+            Id = id;
+            Name = name;
+            
+        }
+        #endregion
+
+
+        #region Methods
+        
+        public void AddSeller(Seller seller) {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
+        #endregion
     }
 }
---
+++ WebSalesMVC/Models/Enums/SaleStatus.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+namespace WebSalesMVC.Models.Enums {
+    public enum SaleStatus : int{
+        
+        //pendente, faturado e cancelado
+        Pending = 0,
+        Billed = 1,
+        Canceled = 2
+
+    }
+}
