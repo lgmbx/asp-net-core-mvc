7ce36e14f08d1a333811166b2f2a82759960bd0b
lgmbx <gustavo_x@live.com>
2020-06-03 21:14:14 -0300

    MySQL adapt and first migration

--- WebSalesMVC/Startup.cs
+++ WebSalesMVC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSalesMVC.Data;
 
+
 namespace WebSalesMVC {
     public class Startup {
         public Startup(IConfiguration configuration) {
@@ -31,8 +32,9 @@ namespace WebSalesMVC {
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-    services.AddDbContext<WebSalesMVCContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("WebSalesMVCContext")));
+            services.AddDbContext<WebSalesMVCContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("WebSalesMVCContext"), builder =>
+            builder.MigrationsAssembly("WebSalesMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
